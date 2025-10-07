81fd554055a5435e640d78d523e8c652dcb706b2
ljvossler <ljvossler@arizona.edu>
2025-10-07 13:37:19 -0700

    removing unneeded files

---
+++ params_msmc_df_CRA_post.sh
@@ -0,0 +1,22 @@
+# msmc specific
+# msmc input generation
+k=150 # For snpability.sh
+prefix=GCF_901933205
+MSMCTOOLS=${PROGDIR}/msmc-tools # directory with msmc-tools binaries
+MSMCDIR=${OUTDIR}/msmc # directory for msmc outputs
+PATH=$PATH:$MSMCTOOLS # add directory with msmc-tools binaries to path
+METHOD=samtools  # or another variant calling method
+
+# msmc run params
+NR_IND=9 # number of individuals in analysis
+#POP_OR_IND=SRR2917338 # name of individual or population being analyzed for script 3
+DATE=`date +%m%d%y`
+RUN_NAME=msmc_${DATE}
+P_PAR=1*2+1*3+8*1+1*2+1*2
+sex_chr=LG9 # name of sex chromosome to omit in analyses
+nchr=`wc -l /xdisk/mcnew/finches/dannyjackson/finches/referencelists/SCAFFOLDS.txt | cut -f1 -d' '`
+THREADS=30
+NUM_OPT=20
+INDEX=0,1,2,3,6,7,8,9,10,11,16,17
+
+echo reading
--- test_results/readme.txt
+++
@@ -1,2 +0,0 @@
-This directory contains test run results for
-a subset of the full finche genome data containing about only one chromosome.
