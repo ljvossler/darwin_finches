5d9e2449029508002c747beb7b52e6805040d90e
ljvossler <ljvossler@arizona.edu>
2025-10-07 13:01:18 -0700

    df specific updates

--- params_msmc_df.sh
+++ params_msmc_df.sh
@@ -1,7 +1,7 @@
 # msmc specific
 # msmc input generation
 k=150 # For snpability.sh
-prefix=GCF_901933205 # Prefix of Reference Genome
+prefix=GCF_901933205
 MSMCTOOLS=${PROGDIR}/msmc-tools # directory with msmc-tools binaries
 MSMCDIR=${OUTDIR}/msmc # directory for msmc outputs
 PATH=$PATH:$MSMCTOOLS # add directory with msmc-tools binaries to path
@@ -9,7 +9,7 @@ METHOD=samtools  # or another variant calling method
 
 # msmc run params
 NR_IND=5 # number of individuals in analysis
-#POP_OR_IND=SRR2917338 # name of individual or population being analyzed for script 3
+POP_OR_IND=SRR2917338 # name of individual or population being analyzed for script 3
 DATE=`date +%m%d%y`
 RUN_NAME=msmc_${DATE}
 P_PAR=1*2+25*1+1*2+1*3 
@@ -17,4 +17,3 @@ sex_chr=LG9 # name of sex chromosome to omit in analyses
 nchr=`wc -l /xdisk/mcnew/finches/dannyjackson/finches/referencelists/SCAFFOLDS.txt | cut -f1 -d' '`
 THREADS=28
 NUM_OPT=20
-MEM=41
--- params_msmc_df_PAR_pre.sh
+++ params_msmc_df_PAR_pre.sh
@@ -8,8 +8,8 @@ PATH=$PATH:$MSMCTOOLS # add directory with msmc-tools binaries to path
 METHOD=samtools  # or another variant calling method
 
 # msmc run params
-NR_IND=7 # number of individuals in analysis
-POP_OR_IND=SRR2917338 # name of individual or population being analyzed for script 3
+NR_IND=12 # number of individuals in analysis
+#POP_OR_IND=SRR2917338 # name of individual or population being analyzed for script 3
 DATE=`date +%m%d%y`
 RUN_NAME=msmc_${DATE}
 P_PAR=1*2+25*1+1*2+1*3 
@@ -17,3 +17,6 @@ sex_chr=LG9 # name of sex chromosome to omit in analyses
 nchr=`wc -l /xdisk/mcnew/finches/dannyjackson/finches/referencelists/SCAFFOLDS.txt | cut -f1 -d' '`
 THREADS=28
 NUM_OPT=20
+INDEX=0,1,2,3,6,7,8,9,20,21,22,23
+
+echo reading
