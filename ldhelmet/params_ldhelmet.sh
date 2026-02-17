33190b54cbb7e3a3e4d5250a70aeb1746320f5cf
ljvossler <ljvossler@arizona.edu>
2026-02-17 13:21:55 -0700

    ldhelmet param updates

--- ldhelmet/params_ldhelmet.sh
+++ ldhelmet/params_ldhelmet.sh
@@ -3,12 +3,12 @@
 micromamba activate ldhelmet_env # If set up using a conda/mamba environment
 source ../params_base_df.sh
 
-INPUT_DIR=${OUTDIR}/datafiles/ldhelmet
+INPUT_DIR=${OUTDIR}/datafiles/ldhelmet/round1
 
 # General Parameters
 THREADS=12
 WINDOW_SIZE=50 # often 50
-MUT_RATE=<SET_VALUE> # Population scaled mutation rate in units of 1/bp
+MUT_RATE=0.00102 # Population scaled mutation rate in units of 1/bp
 
 # Likelihood Lookup Table
 REC_RATE_GRID=0.0 0.1 10.0 1.0 100.0
@@ -18,8 +18,8 @@ PADE_COEF=12
 DEFECT=40
 
 # MCMC Analysis
-BURN_IN=10000
+BURN_IN=100000
 BLOCK_PENALTY=50
-ITERATIONS=100000
+ITERATIONS=1000000
 MUT_MATRIX= # Optional, leave empty if not using a custom mutation matrix. If using, provide the path to the mutation matrix file.
 ANC_PRIOR= # Optional, leave empty if not using ancestral allele priors. If using, provide the path to the ancestral allele prior file. 
